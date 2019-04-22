e2eba63edb46a4ef051a23bd964419def59c6292
mental12345 <ramonesparza@MacBook-Pro-de-Ramon.local>
2019-04-22 12:02:07 -0500

    Initial commit

---
+++ infrastructure/main.tf
@@ -0,0 +1,51 @@
+provider digitalocean {
+    token = "372578ab9ede54402d38ae4bc0f966170930b4b3588f0008a0c8698618c0f3f2"
+}
+
+data "digitalocean_image" "jenkins" {
+    name = "jenkins-image"
+}
+
+data "digitalocean_image" "vault" {
+    name = "vault-image"
+}
+
+data "digitalocean_volume_snapshot" "jenkins_snapshot" {
+  name_regex  = "jenkins*"
+  region      = "${var.region}"
+  most_recent = true
+}
+
+
+resource "digitalocean_volume" "jenkins_volume" {
+  region      = "${var.region}"
+  name        = "jenkins_volume"
+  size        = "${data.digitalocean_volume_snapshot.jenkins_snapshot.min_disk_size}"
+  snapshot_id = "${data.digitalocean_volume_snapshot.jenkins_snapshot.id}"
+}
+
+resource "digitalocean_ssh_key" "default" {
+    name = "default"
+    public_key = "${file("ssh/digitalKey.pub")}"
+}
+
+resource "digitalocean_droplet" "jenkins_droplet" {
+    image  = "${data.digitalocean_image.jenkins.image}"
+    name   = "JenkinsDroplet"
+    region = "${var.region}"
+    size   = "${var.droplet_size}"
+    ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]  
+}
+
+resource "digitalocean_volume_attachment" "foobar" {
+  droplet_id = "${digitalocean_droplet.jenkins_droplet.id}"
+  volume_id  = "${digitalocean_volume.jenkins_volume.id}"
+}
+
+resource "digitalocean_droplet" "vault_droplet" {
+    image  = "${data.digitalocean_image.vault.image}"
+    name   = "VaultDroplet"
+    region = "${var.region}"
+    size   = "${var.droplet_size}"
+    ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
+}
